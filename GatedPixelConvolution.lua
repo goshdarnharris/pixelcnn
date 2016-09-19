6472d906b8c637a63a2162f4c6ee44f0660e0e8c
George Harris <george@luminairecoffee.com>
2016-09-19 16:27:47 -0700

    Complete single-channel implementation of gated pixelcnn layer; untested

--- GatedPixelConvolution.lua
+++ GatedPixelConvolution.lua
@@ -26,64 +26,103 @@ function GAU(planes, left, right)
 	return nn.gModule({input}, {gate})
 end
 
-function GatedPixelConvolution(nInputPlane, nOutputPlane, kernel_size, kernel_step)
+function GatedPixelConvolution(nInputPlane, nOutputPlane, kernel_size, kernel_step, layer, channels, residual)
+	-- nInputPlane and nOutputPlane are per-channel.
+	
+	residual = residual or false
+	layer = layer or 3
+	channels = channels or 1
+
+	-- If we're at the first layer, we can't look at the current pixel.
+	-- If we're at a later layer, we can.
+	-- If we're at the second layer, a channel doesn't look at itself as an input.
+	-- If we're at a later layer, it does.
+
+	-- If nInputPlane and nOutputPlane are the same, force residuals.
+	if nInputPlane == nOutputPlane then residual = true end
+
 	-- TODO: implement dilated convolutions a la wavenet
 
 	-- Vertical stack can only depend on pixels above the current pixel. This can be achieved by 
 	-- using a filter of half height, and padding the input appropriately.
 
-	-- The paper says to use ceil(n/2) x n and ceil(n/2) x 1 convolutions for the horizontal and
-	-- vertical stacks, respectively. I think they mean floor(n/2) x n and floor(n/2) x 1.
-
-	local vertical_conv
+	local vertical_conv, vertical_pad
 	do
 		local kW = kernel_size
 		local kH = math.floor(kernel_size/2) -- Paper says ceil. I don't believe it.
 		local dW = kernel_step
 		local dH = kernel_step
-		local padW = math.floor(kernel_size/2)
-		local padH = math.floor(kernel_size/2) -- Paper says ceil. I don't believe it.
-		vertical_conv = nn.SpatialConvolution(nInputPlane, 2*nOutputPlane, kW, kH, dW, dH, padW, padH)
+		local padW = 0
+		local padH = 0
 
-		-- For an NxN input, the vertical convolution will produce an output of
-		-- (N + 2*floor(kernel_size/2) - kernel_size)/kernel_step + 1
-		-- by
-		-- (N + floor(kernel_size/2))/kernel_step + 1
-		-- for a 3x3 kernel w/ step 1, the output will be N x N+2.
-		-- This means that the output of this convolution needs to be cropped by
-		-- floor(kernel_size/2) + 1 pixels. These pixels should be cropped from the
-		-- bottom of the image.
+		vertical_pad = nn.Sequential()
+		vertical_pad:add(nn.Padding(2, -math.floor(kernel_size/2), 3))
+		vertical_pad:add(nn.Padding(3, -math.floor(kernel_size/2), 3)) -- Paper says ceil. I don't believe it.
+		vertical_pad:add(nn.Padding(3, math.floor(kernel_size/2), 3))
+		vertical_conv = nn.SpatialConvolution(nInputPlane, 2*nOutputPlane, kW, kH, dW, dH, padW, padH)
+		-- We'll have 1 extra pixel on the bottom of the image. Get rid of it.
+		vertical_crop = nn.Narrow(2, 1, -2)
 	end
 
-	local horiz_conv
+	local horiz_conv, horiz_pad
 	do
-		local kW = math.floor(kernel_size/2) -- Paper says ceil.
 		local kH = 1
 		local dW = kernel_step
 		local dH = kernel_step
-		local padW = math.floor(kernel_size/2)
+		local padW = 0
 		local padH = 0
+
+		local kW
+		if layer == 1 then
+			kW = math.floor(kernel_size/2) -- Don't include the current pixel
+		else
+			kW = math.ceil(kernel_size/2) -- Include the current pixel
+		end
+
+		horiz_pad = nn.Padding(3, -math.floor(kernel_size/2), 3)
 		horiz_conv = nn.SpatialConvolution(nInputPlane, 2*nOutputPlane, kW, kH, dW, dH, padW, padH)
 
-		-- Similar to the vertical convolution, the output of the horizonal convolution will need to be cropped.
-		-- They should be cropped from the right side of the image.
+		if layer == 1 then
+			-- Sizes mean we'll still have 1 extra pixel on the right side of the image. Get rid of it.
+			horiz_crop = nn.Narrow(3, 1, -2)
+		else
+			horiz_crop = nn.Identity()
+		end
 	end
 
 	-- Input to the layer is {tensor, tensor}. The first tensor goes to the vertical stack; the second to the
 	-- horizontal stack.
 
-	local vstack_in = - vertical_conv -- Output is nxn vertical convolution
-	local vstack_out = vstack_in - GAU(2*nOutputPlane) -- Output is gated vertical convolution
+	local vstack_in = - vertical_pad -- Output is nxn vertical convolution
+	local vconv_out = vstack_in - vertical_conv - vertical_crop
+	local vstack_out = vconv_out - GAU(2*nOutputPlane) -- Output is gated vertical convolution
 
 	local hstack_in = - nn.Identity()
-	local hconv_out = hstack_in - horiz_conv -- Output is 1xn horizontal convolution
+	local hconv_out = hstack_in - horiz_pad - horiz_conv - horiz_crop -- Output is 1xn horizontal convolution
 
-	local vconv_to_hconv = vstack_in - nn.SpatialConvolution(2*nOutputPlane, 2*nOutputPlane, 1, 1, 1, 1)
-	local hstack = GAU(2*nOutputPlane)(nn.CAddTable()({hconv_out, vconv_to_hconv})) -- - nn.CAddTable() - GAU(2*nOutputPlane)
+	local vconv_to_hconv = vconv_out - nn.SpatialConvolution(2*nOutputPlane, 2*nOutputPlane, 1, 1, 1, 1)
+
+	local hstack = GAU(2*nOutputPlane)(nn.CAddTable()({hconv_out, vconv_to_hconv})) 
+		- nn.SpatialConvolution(nOutputPlane, nOutputPlane, 1, 1, 1, 1)
 	-- hstack output is gated elementwise addition of vconv output (convolved w/ 1x1 filter) and hconv output
 
 	-- Add residual on horizontal stack
-	local hstack_out = nn.CAddTable()({hstack_in, hstack}) -- - nn.CAddTable()
+	local hstack_out
+	if residual then
+		if nInputPlane == nOutputPlane then
+			-- If input and output dimensions are the same, we can just use the identity.
+			hstack_out = nn.CAddTable()({hstack_in, hstack})
+		else
+			-- Otherwise, use a 1x1 convolution to transform dimensions.
+			local transform = hstack_in - nn.SpatialConvolution(nInputPlane, nOutputPlane, 1, 1, 1, 1)
+			hstack_out = nn.CAddTable()({transform, hstack})
+		end
+	else
+		-- hstack output is directly from the stack. no residual.
+		hstack_out = hstack
+	end
+
+	nngraph.annotateNodes()
 
 	return nn.gModule({vstack_in, hstack_in}, {vstack_out, hstack_out})
 end
--- train.lua
+++ train.lua
@@ -3,13 +3,48 @@ require('optim')
 require('nn')
 require('nngraph')
 require('GatedPixelConvolution')
+require('paths')
+local gfx = require('display')
 
-local network = nn.Sequential()
-network:add(GatedPixelConvolution(3, 8, 7, 1))
-network:add(GatedPixelConvolution(8, 8, 3, 1))
 
--- local input = - nn.Identity()
--- local network = input - GatedPixelConvolution(3, 8, 7, 1) - GatedPixelConvolution(8, 8, 3, 1)
--- local network = nn.gModule({input}, {network})
+function infer(network, input)
+	local output
+	paths.rmall(network.name .. ".svg", "yes")
+	local status, err = pcall(function() output = network:forward(input) end)
+	if err then os.execute('open -a "/Applications/Google Chrome.app" ' .. network.name .. '.svg') end
+	return output
+end
 
-print(network:forward({torch.Tensor(3,32,32), torch.Tensor(3,32,32)}))
+nngraph.setDebug(true)
+
+-- graph.dot(GatedPixelConvolution(3, 8, 7, 1).fg, 'gpc')
+
+
+
+local model = GatedPixelConvolution(3, 8, 7, 1, 2)
+model.name = "validate"
+infer(model, {torch.Tensor(3,32,32), torch.Tensor(3,32,32)})
+
+-- local network = nn.Sequential()
+-- network:add(GatedPixelConvolution(3, 8, 7, 1))
+-- network:add(GatedPixelConvolution(8, 8, 3, 1))
+
+local input = - nn.Identity()
+local network = input 
+	- GatedPixelConvolution(3, 8, 7, 1, 1) 
+	- GatedPixelConvolution(8, 8, 3, 1, 2)
+	- GatedPixelConvolution(8, 16, 3, 1, 3)
+	- GatedPixelConvolution(16, 16, 3, 1, 4)
+
+
+
+local model = nn.gModule({input}, {network})
+model.name = "pixelcnn"
+
+print(string.format("model has %d parameters", model:getParameters():size()[1]))
+
+-- print(model:forward({torch.Tensor(3,32,32), torch.Tensor(3,32,32)}))
+
+local input = torch.rand(3,150,150)
+local output = infer(model, {input,input})
+gfx.image(output, {width=500})
